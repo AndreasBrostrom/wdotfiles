9eff894cbc7ebba9b6f519be815c50e859e1599e
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2021-11-18 15:38:43 +0100

    Added configs

---
+++ Home/.pwsh_aliases.ps1
@@ -0,0 +1,28 @@
+
+Set-Alias -Name vim -value nvim -Scope 'Global'
+Set-Alias -Name py -value python -Scope 'Global'
+
+# Git aliases
+function global:gs()    { git status }
+function global:gc()    { git checkout }
+function global:gb()    { git branch }
+function global:gf()    { git fetch --all --prune }
+function global:gr()    { git rebase }
+function global:gu()    { git rebase origin/master }
+
+function global:gp()    { git stash }
+function global:gpp()   { git stash pop }
+
+function global:gpu()   { git push }
+function global:gpuf    { git push fork }
+function global:gpuff   { git push --set-upstream fork $(git rev-parse --abbrev-ref HEAD) }
+
+function global:gfr()   { gf; gr }
+function global:grf()   { gfr }
+function global:gfu()   { gf; gu }
+function global:guf()   { gfu }
+
+function global:gfrp()  { gp; grf }
+function global:gfrpp() { gfrp; gpp }
+
+function global:gcp()   { gp; gc }
---
+++ Home/.pwshrc.ps1
@@ -0,0 +1,53 @@
+# Set prompt title
+$host.ui.RawUI.WindowTitle = "Powershell Core"
+
+# Set prompt to unix like
+function prompt { (Write-Host ("$pwd".replace("$($home)", "~")) -ForegroundColor Blue -NoNewline) + (Write-Host ' PS>' -ForegroundColor DarkGray -NoNewline) + ' '}
+
+# Windows linux conversions
+$curren_path = ($pwd).path
+if (!(Compare-Object "$curren_path" "C:\Windows\system32")) { set-location "$env:userprofile" }
+
+# Windows dir macro extentions
+$FileHiddenPrefix = ".*"
+
+
+if ( Get-Module -ListAvailable -Name Get-ChildItemColor) {
+    if ($PSVersionTable.PSVersion.major -gt 6) {
+        function alias_fnc_ls { Get-ChildItemColorFormatWide $args }
+        Set-Alias -Name ls -Value alias_fnc_ls
+    }
+    function ll { Get-ChildItem $args -Force }
+    function la { Get-ChildItemColorFormatWide $args -Force }
+    function l  { Get-ChildItemColor $args }
+} else {
+    if ($PSVersionTable.PSVersion.major -gt 6) {
+        function alias_fnc_ls { Get-ChildItem $args | Format-Wide -AutoSize }
+        Set-Alias -Name ls -Value alias_fnc_ls
+    }
+    function ll { Get-ChildItem $args -Force}
+    function la { Get-ChildItem $args -Force| Format-Wide -AutoSize }
+    function l  { Get-ChildItem $args }
+}
+
+# Windows Unix extentions
+function reboot { shutdown -r -t 0 $args }
+function ifconfig { ipconfig /all $args }
+
+# Aliases
+if (Test-Path "$env:userprofile\.pwsh_aliases.ps1" -PathType leaf) {
+    . "$env:userprofile\.pwsh_aliases.ps1"
+}
+if (Test-Path "$env:userprofile\.ps_aliases.ps1" -PathType leaf) {
+    . "$env:userprofile\.ps_aliases.ps1"
+}
+
+# Path
+if (Test-Path "$env:userprofile\.pwsh_path.ps1" -PathType leaf) {
+    . "$env:userprofile\.pwsh_path.ps1"
+}
+if (Test-Path "$env:userprofile\.pwsh_path.ps1" -PathType leaf) {
+    . "$env:userprofile\.pwsh_path.ps1"
+}
+
+Invoke-Expression (&starship init powershell)
