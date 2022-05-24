cc162b31173cec4ca11d09d58bae265b313c3609
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-05-24 16:14:24 +0200

    updatd path handler

--- Home/.pwsh_path.ps1
+++ Home/.pwsh_path.ps1
@@ -1,3 +1,13 @@
+# Test paths if so they are not added if exist
+function PATH_TEST([string]$path="")[string]$scope="User" {
+    if ( $path -eq "" ) { return $False }
+    $CurrentPath = [Environment]::GetEnvironmentVariable('Path',$scope)
+    $SplittedPath = $CurrentPath -split ';'
+    if ($SplittedPath.contains("$args")) {
+        return $True
+    }
+    return $False
+}
 
 # Home bin
-$env:Path += ";$env:userprofile\.bin"
+if( PATH_TEST "$env:userprofile\.bin" ) { $env:Path += ";$env:userprofile\.bin" }
