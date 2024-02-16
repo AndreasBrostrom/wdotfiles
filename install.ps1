87776a5bf076bff405748477e45119cd56b06231
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-02-16 10:35:06 +0100

    nounix

--- install.ps1
+++ install.ps1
@@ -5,6 +5,11 @@ Set-Location "$SCRIPT_DIR"
 
 $IS_ADMIN = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 
+if ( $IsLinux ) {
+    Write-Host "Dont run this script when using linux use instead ./install" -ForegroundColor "Red"
+    exit 1
+}
+
 if ( -not $IS_ADMIN ) {
     Write-Host "Not running as admin will copy files instead of linking them."
     Write-Host "Press any key to continue..."
@@ -20,11 +25,7 @@ if ( $IS_ADMIN ) {
         $Fullpath = $_.FullName
         $FilenamePath = $_.FullName | Resolve-Path -Relative
         $FileName = $_.Name
-        if ( $IsLinux ) { # used to block sertain configs in linux assuming .files allready exist.
-            if ( $FileName -eq "starship.toml" )      { return } # continue used for Foreach-Object
-            if ( $FileName -eq ".gitconfig" )         { return } # continue used for Foreach-Object
-            if ( $FileNamePath.Contains('AppData') )  { return } # continue used for Foreach-Object
-        }
+
         Write-Host " Setting up path to $(Split-Path "$HOME/$FilenamePath")" -ForegroundColor "DarkGray"
         New-Item -ItemType "Directory" -Path $(Split-Path "$HOME/$FilenamePath") -Force 2>&1 | out-null
         Write-Host " Creating softlink for $FileName" -ForegroundColor "DarkGray"
@@ -32,11 +33,7 @@ if ( $IS_ADMIN ) {
     }
     
     Set-Location "$SCRIPT_DIR"
-    
-    if ( $IsLinux ) {
-        Write-Host "Nothing more to do on a linux system, we are done!" -ForegroundColor "DarkGreen"
-        exit 0
-    }
+
     
     # Windows ssh
     if (Test-Path -Path "$SCRIPT_DIR/../secure/ssh") {
@@ -55,10 +52,6 @@ if ( $IS_ADMIN ) {
     
     Set-Location "$SCRIPT_DIR"
 } else {
-    if ( $IsLinux ) {
-        Write-Host "Non admin mode cant be used for installing." -ForegroundColor "DarkGreen"
-        exit 0
-    }
     Write-Host "Setting up home" -ForegroundColor "Yellow"
     Set-Location "$SCRIPT_DIR/Home"
     Get-ChildItem -Force -File -Recurse | Foreach-Object {
