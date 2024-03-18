96f370fea38bcb92d76eaa245d5180bdbee7ee03
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-03-18 11:39:25 +0100

    upgrade

--- Home/.config/powershell/conf.d/13_aliases_wsl.ps1
+++ Home/.config/powershell/conf.d/13_aliases_wsl.ps1
@@ -4,12 +4,14 @@ if ( !$IsLinux ) {
     function ubuntu { wsl -d ubuntu $args }
 
     function fish {
+        if ($args.count -eq 0) { wsl -d arch fish; return}
         $distPackageManagers = "eval 'fish -C $args'"
-        Start-Process -NoNewWindow -Wait -FilePath wsl.exe -ArgumentList "--distribution arch", "-- $distPackageManagers"
+        Start-Process -NoNewWindow -Wait -FilePath wsl.exe -ArgumentList "-d arch", "-- $distPackageManagers"
     }
     function bash {
+        if ($args.count -eq 0) { wsl -d arch bash; return}
         $distPackageManagers = "eval 'bash -c $args'"
-        Start-Process -NoNewWindow -Wait -FilePath wsl.exe -ArgumentList "--distribution arch", "-- $distPackageManagers"
+        Start-Process -NoNewWindow -Wait -FilePath wsl.exe -ArgumentList "-d arch", "-- $distPackageManagers"
     }
 
     function cal { wsl -d arch cal $args} 
