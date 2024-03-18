7c8dd709709a52c402c1c49361c51a47bb90c7af
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-03-18 11:48:13 +0100

    added dir check

--- Home/.config/powershell/config.ps1
+++ Home/.config/powershell/config.ps1
@@ -8,12 +8,18 @@ if ( !$IsLinux ) {
 }
 
 # Read Configs
-$configs = Get-ChildItem "$Env:USERPROFILE/.config/powershell/conf.d/" -File -Filter "*.ps1"
-foreach ($config in $configs) { . "$config" }
+$configDir = "$Env:USERPROFILE/.config/powershell/conf.d//"
+if (Test-Path -Path $configDir) {
+    $configs = Get-ChildItem "$Env:USERPROFILE/.config/powershell/conf.d/" -File -Filter "*.ps1"
+    foreach ($config in $configs) { . "$config" }
+}
 
 # Read Functions
-$functions = Get-ChildItem "$Env:USERPROFILE/.config/powershell/functions/" -File -Filter "*.ps1"
-foreach ($function in $functions) { . "$function" }
+$functionDir = "$Env:USERPROFILE/.config/powershell/functions/"
+if (Test-Path -Path $functionDir) {
+    $functions = Get-ChildItem $functionDir -File -Filter "*.ps1"
+    foreach ($function in $functions) { . "$function" }
+}
 
 $Env:STARSHIP_CONFIG = "$Env:LOCALAPPDATA/starship_pwsh.toml"
 Invoke-Expression (&starship init powershell)
