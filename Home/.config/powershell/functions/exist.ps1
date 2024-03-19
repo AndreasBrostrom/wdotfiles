0b864f8d8c091f58f355f938a7a935cf11611aa6
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-03-19 10:58:52 +0100

    Only apps now

--- Home/.config/powershell/functions/exist.ps1
+++ Home/.config/powershell/functions/exist.ps1
@@ -1,7 +1,7 @@
 function exist {
     if ( -not $args[0] ) { return $false }
     
-    if (Get-Command $args[0] -errorAction SilentlyContinue) {
+    if (Get-Command $args[0] -CommandType "Application" -errorAction SilentlyContinue) {
         return $true
     } else {
         return $false
