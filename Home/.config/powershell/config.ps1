68e24584ccbed56fb8045ba21a9eb6f28668b7a7
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-07-18 23:12:05 +0200

    Fixed powershell version compatability

--- Home/.config/powershell/config.ps1
+++ Home/.config/powershell/config.ps1
@@ -8,18 +8,32 @@ if ( !$IsLinux ) {
 }
 
 # Read Functions
-$functionDir = "$Env:USERPROFILE/.config/powershell/functions/"
+$functionDir = "$Env:USERPROFILE\.config\powershell\functions\"
 if (Test-Path -Path $functionDir) {
     $functions = Get-ChildItem $functionDir -File -Filter "*.ps1"
-    foreach ($function in $functions) { . "$function" }
+    foreach ($function in $functions) {
+        if ($PSVersionTable.PSVersion.Major -gt 5) {
+            . "$function"
+        } else {
+            $fullFuncPath = join-path $functionDir $function
+            . "$fullFuncPath"
+        }
+    }
 }
 
 # Read Configs
-$configDir = "$Env:USERPROFILE/.config/powershell/conf.d/"
+$configDir = "$Env:USERPROFILE\.config\powershell\conf.d\"
 if (Test-Path -Path $configDir) {
     $configs = Get-ChildItem $configDir -File -Filter "*.ps1"
-    foreach ($config in $configs) { . "$config" }
+    foreach ($config in $configs) {
+        if ($PSVersionTable.PSVersion.Major -gt 5) {
+            . "$config"
+        } else {
+            $fullConfPath = join-path $configDir $config
+            . "$fullConfPath"
+        }
+    }
 }
 
-$Env:STARSHIP_CONFIG = "$Env:LOCALAPPDATA/starship_pwsh.toml"
+$Env:STARSHIP_CONFIG = "$Env:LOCALAPPDATA\starship_pwsh.toml"
 Invoke-Expression (&starship init powershell)
