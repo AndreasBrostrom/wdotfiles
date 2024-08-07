4a1b521d726e4e686e5fc7ad9da50829c17065bf
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-08-07 14:25:00 +0200

    Updated settings as well as added module

--- Home/.config/powershell/config.ps1
+++ Home/.config/powershell/config.ps1
@@ -35,5 +35,8 @@ if (Test-Path -Path $configDir) {
     }
 }
 
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+
 $Env:STARSHIP_CONFIG = "$Env:LOCALAPPDATA\starship_pwsh.toml"
 Invoke-Expression (&starship init powershell)
