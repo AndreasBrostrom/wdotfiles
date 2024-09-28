24f76f92019a20296c6355d46a17efc48f052888
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-09-28 08:17:32 +0200

    Ignore starting of WinGet.CommandNotFound if on powershell

--- Home/.config/powershell/config.ps1
+++ Home/.config/powershell/config.ps1
@@ -35,8 +35,10 @@ if (Test-Path -Path $configDir) {
     }
 }
 
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-Import-Module -Name Microsoft.WinGet.CommandNotFound
+if ($PSVersionTable.PSVersion.major -gt 6) {
+    #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+    Import-Module -Name Microsoft.WinGet.CommandNotFound
+}
 
 $Env:STARSHIP_CONFIG = "$Env:LOCALAPPDATA\starship_pwsh.toml"
 Invoke-Expression (&starship init powershell)
