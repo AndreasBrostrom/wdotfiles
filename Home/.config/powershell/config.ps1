171d4172506c45d741f9343e6cc94d33592d3992
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-10-15 11:15:43 +0200

    dont run if linux

--- Home/.config/powershell/config.ps1
+++ Home/.config/powershell/config.ps1
@@ -34,8 +34,8 @@ if (Test-Path -Path $configDir) {
         }
     }
 }
-
-if ($PSVersionTable.PSVersion.major -gt 6) {
+ 
+if (!$IsLinux -and $PSVersionTable.PSVersion.major -gt 6) {
     #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
     Import-Module -Name Microsoft.WinGet.CommandNotFound
 }
